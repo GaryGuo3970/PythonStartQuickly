7d84ea446f849c102c43ff7727eca7e8c89e8679
garyguo <gary.guo@incadea.com>
2025-02-25 00:48:41 +0800

    Chapter 5,6: dic and string

---
+++ C5_Dic.py
@@ -0,0 +1,30 @@
+import pprint
+
+myInfo={"Name":"Gary","Age":10,"Address":"Shanghai","Salary":12345.6}
+print(myInfo["Age"])
+print(myInfo["Address"])
+
+#keys
+for k in myInfo.keys():
+    print(k+":"+str(myInfo[k]))
+
+#Values
+for v in myInfo.values():
+    print(str(v))
+
+#items
+for k,v in myInfo.items():
+    print(str(k)+"-"+str(v))
+
+#get 获取字典的值，如果不存在，就返回一个默认值
+print(myInfo.get("Age",0))  
+print(myInfo.get("Age2",0))
+
+#嵌套
+spam={"item1":{"Name":"Gary","Age":10,"Address":"Shanghai","Salary":12345.6},"item2":{"Name":"Peter","Age":22,"Address":"Bejing","Salary":65432}}
+for k,v in spam.items():
+    print("------------")
+    print("|"+str(k)+"|")
+    print("------------")
+    for subk,subv in v.items():
+        print(str(subk)+"|"+str(subv))
---
+++ C6_String.py
@@ -0,0 +1,13 @@
+spam="\"That is Demo's cat\" \nNext Line \n age is 3"
+print(spam)
+print(spam.isalnum()) #是否只包含数字
+print(spam.isalpha())
+
+
+#rjust 往右移多少位置
+spam = spam+"RIGHT".rjust(10)
+print(spam)
+#rjust 往左移多少位置
+spam = spam+"LEFT".ljust(10)
+print(spam)
+print("Hello".center(20,"="))
